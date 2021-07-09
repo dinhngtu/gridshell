a1712a6bd43aef3cc15bf3f09daaccd9b208c3bb
Tu Dinh <dinhngtu@users.noreply.github.com>
2021-07-10 01:12:54 +0200

    more user cmdlets

--- public/Users.ps1
+++ public/Users.ps1
@@ -11,30 +11,38 @@ Export-ModuleMember -Function Get-G5KUser
 function Get-G5KGroup {
     [CmdletBinding()]
     param(
-        [Parameter(Position = 0)]$GroupName,
-        [Parameter(Mandatory, ValueFromPipelineByPropertyName)][ValidatePattern("\w*")]$Cluster,
-        [Parameter(ValueFromPipelineByPropertyName)][ValidatePattern("\w*")]$Site,
-        [Parameter()][ValidatePattern("\w*")]$Branch = "master",
-        [Parameter(ParameterSetName = "Date")][System.Nullable[datetime]]$Date,
-        [Parameter(ParameterSetName = "UnixTimestamp")][System.Nullable[long]]$UnixTimestamp,
-        [Parameter(ParameterSetName = "Version")][string]$Version,
+        [Parameter(Position = 0)][string]$GroupName,
         [Parameter()][pscredential]$Credential
     )
     if (!$Site) {
         $Site = Get-G5KCurrentSite
     }
-    $params = Remove-EmptyValues @{
-        branch    = $Branch;
-        date      = $Date ? $Date.ToString("s") : $null;
-        timestamp = $UnixTimestamp;
-        version   = $Version;
-    }
-    $resp = Invoke-RestMethod -Uri ("{0}/3.0/sites/{1}/clusters/{2}/nodes/{3}" -f $g5kApiRoot, $Site, $Cluster, $Node) -Credential $Credential -Body $params
-    if ($Node) {
-        return $resp | ConvertTo-OarNodeObject
+    $resp = Invoke-RestMethod -Uri ("{0}/3.0/users/groups/{1}" -f $g5kApiRoot, $GroupName) -Credential $Credential
+    if ($GroupName) {
+        return $resp
     }
     else {
-        return $resp.items | ConvertTo-OarNodeObject
+        return $resp.items
     }
 }
-Export-ModuleMember -Function Get-OarNode
+Export-ModuleMember -Function Get-G5KGroup
+
+function Get-G5KUserGroups {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0)][ValidatePattern("\w*")]$UserName = "~",
+        [Parameter()][pscredential]$Credential
+    )
+    return (Invoke-RestMethod -Uri ("{0}/3.0/users/users/{1}/groups" -f $g5kApiRoot, $UserName) -Credential $Credential).items
+}
+Export-ModuleMember -Function Get-G5KUserGroups
+
+function Get-G5KGroupMembers {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position = 0)][ValidatePattern("\w*")]$GroupName,
+        [Parameter()][pscredential]$Credential
+    )
+    return (Invoke-RestMethod -Uri ("{0}/3.0/users/groups/{1}/members" -f $g5kApiRoot, $GroupName) -Credential $Credential).items
+}
+Export-ModuleMember -Function Get-G5KGroupMembers
