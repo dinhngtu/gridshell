60309ae36f88639bc39c6957fdacf0fd4129b42a
Tu Dinh <dinhngtu@users.noreply.github.com>
2021-07-09 22:00:04 +0200

    kwollect tweaks

--- public/Kwollect.ps1
+++ public/Kwollect.ps1
@@ -1,22 +1,27 @@
 function Get-KwollectMetrics {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory)][ValidatePattern("\w*")]$Site,
+        [Parameter()][ValidatePattern("\w*")]$Site,
         [Parameter()][string[]]$Nodes,
         [Parameter()][System.Nullable[datetime]]$StartTime,
         [Parameter()][System.Nullable[datetime]]$EndTime,
         [Parameter()][int]$JobId,
         [Parameter()][string[]]$Metrics,
+        [Parameter()][switch]$Summary,
         [Parameter()][string]$OutFile,
         [Parameter()][pscredential]$Credential
     )
+    if (!$Site) {
+        $Site = Get-G5KCurrentSite
+    }
     $params = Remove-EmptyValues @{
         nodes      = $Nodes.Count -gt 0 ? ($Nodes -join ",") : "";
         start_time = $StartTime ? $StartTime.ToString("s") : "";
         end_time   = $EndTime ? $EndTime.ToString("s") : "";
         job_id     = $PSBoundParameters.ContainsKey("JobId") ? $JobId : "";
         metrics    = $Metrics.Count -gt 0 ? ($Metrics -join ",") : "";
+        summary    = $Summary ? $true : $false;
     }
-    return Invoke-RestMethod -Uri ("{0}/sid/sites/{1}/metrics" -f $g5kApiRoot, $Site) -Credential $Credential -Body $params -OutFile $OutFile
+    return Invoke-RestMethod -Uri ("{0}/3.0/sites/{1}/metrics" -f $g5kApiRoot, $Site) -Credential $Credential -Body $params -OutFile $OutFile
 }
 Export-ModuleMember -Function Get-KwollectMetrics
